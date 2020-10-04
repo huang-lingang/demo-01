aca2c4daff71e42d3c80a3a41a146b56fce47516
huang-lingang <lingang_huang@163.com>
2020-10-04 10:49:14 +0800

    add test

--- src/main/java/com/example/demo01/Controller/HelloController.java
+++ src/main/java/com/example/demo01/Controller/HelloController.java
@@ -3,7 +3,8 @@ package com.example.demo01.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+//test
 public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
